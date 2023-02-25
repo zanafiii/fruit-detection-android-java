47d9ac1ab41229076f4ba24dfef3dc34cb5c1940
Azzam Hanafi <azzam.emirates@gmail.com>
2023-02-26 00:54:59 +0700

    first commit

---
+++ app/src/main/java/com/azzamhanafi/myfruit/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.azzamhanafi.myfruit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
